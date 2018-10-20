5743f98d1eb8378ffd8a300fc01907ea22892cbd
kokoseda <kokoseda22@yahoo.com>
2018-10-20 17:06:46 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('ready', async() => {
+var server = "487652334449590272"; // ايدي السررفر
+var channel = "487652334449590274";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('** فرعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفؤعونفرعونفرعونفؤعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعومفرعون **')
+    },305);
+})
+
+
+
+client.on('ready', async() => {
+var server = "487652334449590272"; // ايدي السررفر
+var channel = "487652334449590274";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+
+client.on('ready', async() => {
+var server = "487652334449590272"; // ايدي السررفر
+var channel = "487652334449590274";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    },86400);
+})
+
+
+
+client.login(process.env.BOT_TOKEN); 
